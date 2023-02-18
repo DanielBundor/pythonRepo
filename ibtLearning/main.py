720bceb7bee623bf9d6b997cc7fc2cd2858012ae
Daniel Bundor <danielbundor91@gmail.com>
2023-02-17 23:41:48 -0600

    adding python repos and files

---
+++ ibtLearning/main.py
@@ -0,0 +1,29 @@
+'''
+
+This program will print my name: Daniel
+
+'''
+
+# age = 15
+#
+# if age > 25:
+#     print("You are a young adult")
+# elif age > 50:
+#     print("You are on your way to a senior")
+# elif age > 25 and age < 50:
+#     print("You are an adult")
+# elif age < 25:
+#     print("You are really young")
+
+# player_1 = "rock"
+# player_2 = "paper"
+#
+# if player_1 == 'rock':
+#     if player_2 == 'scissors':
+#         print("Player 1 won.")
+#     elif player_2 == 'paper':
+#         print("Player 2 won.")
+#     else:
+#         print("Game ended in a draw.")
+
+
